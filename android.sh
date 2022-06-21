2d50334c9da8b372605e8fed58ad34b518613d5e
xianX1 <53300365+xianX1@users.noreply.github.com>
2022-06-22 07:02:50 +0800

    Create android.sh

---
+++ android.sh
@@ -0,0 +1,45 @@
+# shellcheck disable=SC1113
+
+#/bin/bash
+
+updateAndRun(){
+
+  cd chaoxing_auto_flash || exit
+
+   git pull
+
+   npm install
+
+   node main.js
+
+}
+
+echo "android命令行一键脚本"
+
+if [  -d chaoxing_auto_flash ]
+
+then
+
+   echo "脚本已经存在，检查更新并运行"
+
+   updateAndRun
+
+else
+
+echo "脚本未下载，开始下载"
+
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+
+apt update
+
+yes n | apt upgrade -y
+
+apt install nodejs wget git -y
+
+git clone https://gh.fakev.cn/lyj0309/chaoxing-xuexitong-autoflush.git
+
+npm config set registry https://registry.npm.taobao.org/
+
+updateAndRun
+
+fi
